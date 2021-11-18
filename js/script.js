7a5fde73607b6a190e812cc60fd2c3c66010bc35
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2021-11-18 17:40:42 +0100

    fix bombs

--- js/script.js
+++ js/script.js
@@ -7,55 +7,84 @@ const bombsNumber = 16;
 container.innerHTML = '';
 
 let cellNumber = 100;
+
+let bombs = generateBombs();
+console.log(bombs);
+
 for (let i = 0; i < 100; i++) {
     const cell = document.createElement('div');
     cell.className = 'square easy';
     cell.innerHTML = i + 1;
     cell.addEventListener('click', function () {
+        if (bombs.includes(parseInt(this.innerHTML))) {
+            this.classList.add('red');
+        } else {
         cell.classList.add('clicked-true');
+        }
     });
+    
     container.appendChild(cell);
 }
 
 buttonGioca.addEventListener("click", function () {
     
     container.innerHTML = '';
+    bombs = []
+    console.log(bombs);
 
     const difficulty = document.getElementById('difficulty').value;
 
     switch (difficulty) {
         case "easy":
             cellNumber = 100;
+            bombs = generateBombs();
+            console.log(bombs);
             for (let i = 0; i < cellNumber; i++) {
                 const cell = document.createElement('div');
                 cell.className = 'square easy';
                 cell.innerHTML = i + 1;
                 cell.addEventListener('click', function () {
-                    cell.classList.add('clicked-true');
+                    if (bombs.includes(parseInt(this.innerHTML))) {
+                        this.classList.add('red');
+                    } else {
+                        cell.classList.add('clicked-true');
+                    }
                 });
                 container.appendChild(cell);
             }
             break;
         case "hard":
             cellNumber = 81;
+            bombs = generateBombs();
+            console.log(bombs);
             for (let i = 0; i < cellNumber; i++) {
                 const cell = document.createElement('div');
                 cell.className = 'square hard';
                 cell.innerHTML = i + 1;
                 cell.addEventListener('click', function () {
-                    cell.classList.add('clicked-true');
+                    if (bombs.includes(parseInt(this.innerHTML))) {
+                        this.classList.add('red');
+                    } else {
+                        cell.classList.add('clicked-true');
+                    }
                 });
                 container.appendChild(cell);  
             }
             break;
         case "expert":
             cellNumber = 49;
+            bombs = generateBombs();
+            console.log(bombs);
             for (let i = 0; i < cellNumber; i++) {
                 const cell = document.createElement('div');
                 cell.className = 'square expert';
                 cell.innerHTML = i + 1;
                 cell.addEventListener('click', function(){
-                    cell.classList.add('clicked-true');
+                    if (bombs.includes(parseInt(this.innerHTML))) {
+                        this.classList.add('red');
+                    } else {
+                        cell.classList.add('clicked-true');
+                    }
                 });
                 container.appendChild(cell);
 
@@ -64,31 +93,17 @@ buttonGioca.addEventListener("click", function () {
     }
 });
 
+function generateBombs() {
+    const arrayBombs = [];
+    while (arrayBombs.length < bombsNumber) {
+        const numeroRandom = getRndInteger(1, cellNumber);
+        if (!arrayBombs.includes(numeroRandom)) {
+            arrayBombs.push(numeroRandom);
+        }
+    }
+    return arrayBombs;
+}
 
-// const bombs = generateBombs();
-// console.log(bombs);
-
-// function generateBombs() {
-//     const arrayBombs = [];
-//     while (arrayBombs.length < bombsNumber) {
-//         const numeroRandom = getRndInteger(1, cellNumber);
-//         if (!arrayBombs.includes(numeroRandom)) {
-//             arrayBombs.push(numeroRandom);
-//         }
-//     }
-//     return arrayBombs;
-// }
-
-// function getRndInteger(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-// const square = document.querySelector('.square');
-
-// for (let i = 1; i <= cellNumber; i++) {
-//     if (i == bombs[i]) {
-//         square[i].addEventListener('click', function () {
-//             square[i].classList.add('red');
-//         });
-//     }
-// }
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
